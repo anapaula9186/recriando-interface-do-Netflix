3d7bbeff9b76ce9cb6a2b341d2c38dadd8253cb8
unknown <ana.paula.91@hotmail.com>
2020-11-28 16:57:05 -0300

    recriando interface netflix

---
+++ js/owl/trailers.js
@@ -0,0 +1,8 @@
+function filme7(elemento){
+    window.open("/trailers.html");
+  window.open("/trailers-lista.html");
+}
+function playfilme7(){
+var player = document.getElementById("filme7").filmeplay.innerHTML=(player);
+}
+
